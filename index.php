58f9c786b63c2eccbcca18e2cc22d39467b70ada
cforsythe <cforsythe@csumb.edu>
2017-08-29 03:10:46 +0000

    Added homepage

--- index.php
+++ index.php
@@ -5,6 +5,33 @@
         <meta charset="utf-8"/>
     </head>
     <body>
-        <h1> Hello World</h1>
+        <header>
+            <h1>Christopher Forsythe</h1>
+        </header>
+        <nav>
+            <hr width='50%'/>
+            <a href='index.html'>Home</a>
+            <a href='about.html'>About</a>
+            <a href='contact.html'>Contact me</a>
+        </nav>    
+        <br/><br/>
+        <main>
+            <figure> 
+                <img src='img/me.png' alt='Picture of Christopher'/>
+            </figure>
+            <div>
+                Hello!
+                <br/>
+                <p>Thank's for taking a look at my professional website!</p>
+                <p>I am a student at CSUMB majoring in Computer Science with a Software Engineering concentration.</p>
+                <p>Feel free to contact me.</p>
+                <br/><br/>
+                <em>.To know even <strong>one</strong> life has breathed easier because you have lived. This is to have succeeded.</em>
+                <br/>
+                - Ralph Waldo Emerson
+            </div>
+            
+        </main>
+        
     </body>
 </html>
