da5c371a8fb510263a80eaa18cbdf70bdbf27886
cforsythe <cforsythe@csumb.edu>
2017-10-15 07:35:52 +0000

    fixed db bug

--- dbConnection.php
+++ dbConnection.php
@@ -6,7 +6,7 @@ function getDatabaseConnection(){
     $username = 'root';
     $password = 'mypass';
     
-    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+    if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') != false){
         $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
         $host = $url["host"];
         $dbname = substr($url["path"], 1);
