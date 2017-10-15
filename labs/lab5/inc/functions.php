2333684843c700191d4910a82e1151b0f11a8a28
cforsythe <cforsythe@csumb.edu>
2017-10-15 07:09:37 +0000

    lab5 done

--- labs/lab5/inc/functions.php
+++ labs/lab5/inc/functions.php
@@ -1,5 +1,64 @@
 <?php
+include '../../dbConnection.php';
+
+
+$conn = getDatabaseConnection();
+
 function getDeviceTypes(){
+    global $conn;
+    $sql = "SELECT DISTINCT(deviceType)
+            FROM `tc_device` 
+            ORDER BY deviceType";
     
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    foreach ($records as $record){
+        echo "<option> "  . $record['deviceType'] . "</option>";
+    }
+}
+
+
+function displayDevices(){
+    global $conn;
+    
+    $sql = "SELECT * FROM tc_device WHERE 1";
+    
+    
+    if(isset($_GET['submit'])){
+        $namedParameters = array();
+        if(!empty($_GET['deviceName'])){
+            $sql .= " AND deviceName LIKE :deviceName"; 
+            $namedParameters[':deviceName'] = "%" . $_GET['deviceName'] . "%";
+        }
+         
+        if(!empty($_GET['deviceType']) && $_GET['deviceType'] != 'Select Category'){
+            $sql .= " AND deviceType = :dType";
+            $namedParameters[':dType'] =   $_GET['deviceType'];
+        }
+        if(isset($_GET['available'])){
+            $sql .= " AND status LIKE :availability";
+            $namedParameters[':availability'] = "%" . "a" . "%";
+        }
+        if(isset($_GET['orderBy'])){
+            if($_GET['orderBy'] == 'name'){
+                $sql .= " ORDER BY deviceName ASC";
+            }
+            else{
+                $sql .= " ORDER BY price ASC";
+            }
+        }
+    }
+    
+    $stmt = $conn->prepare($sql);
+    $stmt->execute($namedParameters);
+    $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    
+    foreach ($records as $record){
+        echo  $record['deviceName'] . " " . $record['deviceType'] . " " .
+              $record['price'] .  "  " . $record['status'] . 
+              "<a target='checkoutHistory' href='checkoutHistory.php?deviceId=".$record['deviceId']."'> Checkout History </a> <br />";
+    }
 }
 ?>
--- labs/lab5/index.php
+++ labs/lab5/index.php
@@ -6,17 +6,36 @@ include 'inc/functions.php';
 <html>
     <head>
         <title>Technology Center</title>
+        <link rel='stylesheet' href='css/styles.css'>
     </head>
     <body>
-        <h1>Technology Center: Checkout System</h1>
-        
-        <form>
-            Device: <input type='text' name='deviceName' placeholder='Device Name'/>
-            Type:
-            <select name='deviceType'>
-                <?=getDeviceTypes()?>
-            </select>
+        <div class='search'>
+            <h1>Technology Center: Checkout System</h1>
             
-        </form>
+            <form>
+                Device: <input type='text' name='deviceName' placeholder='Device Name'/>
+                Type:
+                <select name='deviceType'>
+                    <option>Select Category</option>
+                    <?=getDeviceTypes()?>
+                </select>
+                
+                <input type="checkbox" name="available" id="available">
+                <label for="available"> Available </label>
+                <br>
+                Order by:
+                <input type="radio" name="orderBy" id="orderByName" value="name"/> 
+                <label for="orderByName"> Name </label>
+                <input type="radio" name="orderBy" id="orderByPrice" value="price"/> 
+                <label for="orderByPrice"> Price </label>
+                </br>
+                <input type="submit" value="Search!" name="submit" >
+            </form>
+        </div>
+        
+         <hr>
+        
+        <?=displayDevices()?>
+        <iframe name="checkoutHistory" width="400" height="400"></iframe>
     </body>
 </html>
